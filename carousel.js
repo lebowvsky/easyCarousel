33e4cf68ced1db54069d83a2d6db4731e2faab98
lebowvsky <lebowvsky@gmail.com>
2020-03-22 23:49:03 +0100

    The first step of my new carousel, img appear and disappear every 3 seconds

---
+++ carousel.js
@@ -0,0 +1,27 @@
+
+
+const container = document.body.querySelector('#container');
+const imagesCarousel = document.body.querySelectorAll('.imgCarousel');
+
+
+/* Flex */
+container.style.display = "flex";
+container.style.justifyContent = "center";
+
+imagesCarousel.forEach(image => {
+    image.style.display = "none";
+    
+})
+
+let i=0;
+
+setInterval(()=> {
+    let j=i-1
+    j<0 ? j = imagesCarousel.length-1 : j;
+    imagesCarousel[j].style.display = "none";
+    imagesCarousel[i].style.display = "block";
+    imagesCarousel[i].style.width = "auto";
+    i == imagesCarousel.length-1 ? i=0 : i++;
+}, 3000);
+
+
