b1a3fc465cfd135d0f3b514e641a40af2f05d2f4
lebowvsky <lebowvsky@gmail.com>
2020-03-23 01:14:19 +0100

    alternative apparitions from top to bottom and bottom to top...

--- carousel.js
+++ carousel.js
@@ -8,7 +8,7 @@ const imagesCarousel = document.body.querySelectorAll('.imgCarousel');
 container.style.display = "flex";
 container.style.justifyContent = "center";
 
-imagesCarousel.forEach(image => {
+/* imagesCarousel.forEach(image => {
     image.style.display = "none";
     
 })
@@ -22,6 +22,23 @@ setInterval(()=> {
     imagesCarousel[i].style.display = "block";
     imagesCarousel[i].style.height = "100%";
     i == imagesCarousel.length-1 ? i=0 : i++;
+}, 3000); */
+
+imagesCarousel.forEach((image, index) => {
+    image.style.height = "0";
+    image.style.position = "absolute";
+    index%2==0 ? imagesCarousel[index].style.top = "0" : imagesCarousel[index].style.bottom = "0";  //une photo sur deux a sa position top pour transition plus fluide (penser à gérer le cas nbre total photo est impair)
+    image.style.transition = "all 350ms ease-in-out";
+    
+})
+
+let i=0;
+imagesCarousel[i].style.top = "0";
+imagesCarousel[i].style.height = "100%";
+setInterval(()=> {
+    let j=i-1
+    j<0 ? j = imagesCarousel.length-1 : j;
+    imagesCarousel[j].style.height = "0";
+    imagesCarousel[i].style.height = "100%";
+    i == imagesCarousel.length-1 ? i=0 : i++;
 }, 3000);
-
-
