422be26543ebeacf4c5e7f83b89467e0acff7436
lebowvsky <lebowvsky@gmail.com>
2020-03-29 03:49:50 +0200

    Creation of a side to side motion

--- README.md
+++ README.md
@@ -3,3 +3,11 @@ A simple carousel easy to use
 
 Github Pages :
 https://lebowvsky.github.io/easyCarousel/index.html
+
+
+create a box with an "id='container'" and some images inside with the "class='imgCarousel'".
+
+You can choose the motion of your images with a class on the container.
+There's 2 styles :
+-with an alternate motion from side to side with the :  class='side-to-side'
+-with an alternate motion from top to bottom with the : class='top-to-btm'
--- carousel.js
+++ carousel.js
@@ -8,37 +8,51 @@ const imagesCarousel = document.body.querySelectorAll('.imgCarousel');
 container.style.display = "flex";
 container.style.justifyContent = "center";
 
-/* imagesCarousel.forEach(image => {
-    image.style.display = "none";
-    
-})
 
-let i=0;
 
-setInterval(()=> {
-    let j=i-1
-    j<0 ? j = imagesCarousel.length-1 : j;
-    imagesCarousel[j].style.display = "none";
-    imagesCarousel[i].style.display = "block";
+
+
+
+if(container.className == 'top-to-btm'){
+
+    imagesCarousel.forEach((image, index) => {
+        image.style.height = "0";
+        image.style.position = "absolute";
+        index%2==0 ? imagesCarousel[index].style.top = "0" : imagesCarousel[index].style.bottom = "0";  //une photo sur deux a sa position top pour transition plus fluide (penser à gérer le cas nbre total photo est impair)
+        image.style.transition = "all 350ms ease-in-out";   
+    })
+
+    let i=0;
+    imagesCarousel[i].style.top = "0";
     imagesCarousel[i].style.height = "100%";
-    i == imagesCarousel.length-1 ? i=0 : i++;
-}, 3000); */
+    setInterval(()=> {
+        let j=i-1
+        j<0 ? j = imagesCarousel.length-1 : j;
+        imagesCarousel[j].style.height = "0";
+        imagesCarousel[i].style.height = "100%";
+        i == imagesCarousel.length-1 ? i=0 : i++;
+}   , 3000);
 
-imagesCarousel.forEach((image, index) => {
-    image.style.height = "0";
-    image.style.position = "absolute";
-    index%2==0 ? imagesCarousel[index].style.top = "0" : imagesCarousel[index].style.bottom = "0";  //une photo sur deux a sa position top pour transition plus fluide (penser à gérer le cas nbre total photo est impair)
-    image.style.transition = "all 350ms ease-in-out";
-    
-})
+} else if(container.className == 'side-to-side'){
 
-let i=0;
-imagesCarousel[i].style.top = "0";
-imagesCarousel[i].style.height = "100%";
-setInterval(()=> {
-    let j=i-1
-    j<0 ? j = imagesCarousel.length-1 : j;
-    imagesCarousel[j].style.height = "0";
-    imagesCarousel[i].style.height = "100%";
-    i == imagesCarousel.length-1 ? i=0 : i++;
-}, 3000);
+    imagesCarousel.forEach((image, index) => {
+        image.style.width = "0";
+        /* image.style.height = "100%"; */
+        image.style.position = "absolute";
+        index%2==0 ? imagesCarousel[index].style.left = "0" : imagesCarousel[index].style.right = "0";  //une photo sur deux a sa position gauche pour transition plus fluide (penser à gérer le cas nbre total photo est impair)
+        image.style.transition = "all 350ms ease-in-out";   
+    })
+
+    let i=0;
+    imagesCarousel[i].style.top = "left";
+    imagesCarousel[i].style.width = "100%";
+    setInterval(()=> {
+        let j=i-1
+        j<0 ? j = imagesCarousel.length-1 : j;
+        imagesCarousel[j].style.width = "0";
+        imagesCarousel[i].style.width = "100%";
+        i == imagesCarousel.length-1 ? i=0 : i++;
+    }   , 3000);
+}
+
+
